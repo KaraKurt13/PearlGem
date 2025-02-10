c5a2800afc4f7431fd209a44f2803c2c1490076e
KaraKurt <penguin.ners@gmail.com>
2025-02-10 17:59:42 +0200

    Level generator

--- Assets/Scripts/Main/Engine.cs
+++ Assets/Scripts/Main/Engine.cs
@@ -14,11 +14,7 @@ namespace Assets.Scripts.Main
         public LevelGenerator LevelGenerator;
 
         public GameObject hexPrefab;
-        public List<SphereElement> sphereElements = new List<SphereElement>();
 
-
-        private List<Vector3> hexCenters = new List<Vector3>();
-        private Dictionary<Vector3, List<Vector3>> neighbors = new Dictionary<Vector3, List<Vector3>>();
 
         private void Update()
         {
@@ -39,35 +35,7 @@ namespace Assets.Scripts.Main
 
         void Start()
         {
-            GenerateHexGrid();
-            DrawHexCenters();
-        }
-
-        void GenerateHexGrid()
-        {
-            hexCenters = MathHelper.GenerateIcospherePoints(5, sphereRadius);
-        }
-
-        void DrawHexCenters()
-        {
-            foreach (var center in hexCenters)
-            {
-                var element = Instantiate(hexPrefab, center, Quaternion.identity, transform).GetComponent<SphereElement>();
-                element.Center = center;
-                sphereElements.Add(element);
-            }
-            CalculateNeighbors();
-        }
-
-        private void CalculateNeighbors()
-        {
-            foreach (var element in sphereElements)
-            {
-                var center = element.Center;
-                element.Neighbours = sphereElements
-                    .Where(other => other != element && Vector3.Distance(center, other.Center) <= 1.1f)
-                    .ToList();
-            }
+            LevelGenerator.Generate(5);
         }
     }
 }
--- Assets/Scripts/Main/LevelGenerator.cs
+++ Assets/Scripts/Main/LevelGenerator.cs
@@ -1,11 +1,48 @@
+using Assets.Scripts.Helpers;
+using Assets.Scripts.Objects;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Assets.Scripts.Main
 {
     public class LevelGenerator : MonoBehaviour
     {
+        private List<Vector3> _hexCenters;
 
+        private List<SphereElement> _sphereElements;
+
+        [SerializeField]
+        private GameObject _spherePrefab;
+
+        public void Generate(int radius)
+        {
+            _hexCenters = MathHelper.GenerateIcospherePoints(5, radius);
+            DrawSphereElements();
+            CalculateNeighbours();
+        }
+
+        private void DrawSphereElements()
+        {
+            _sphereElements = new();
+            foreach (var center in _hexCenters)
+            {
+                var element = Instantiate(_spherePrefab, center, Quaternion.identity, transform).GetComponent<SphereElement>();
+                element.Center = center;
+                _sphereElements.Add(element);
+            }
+        }
+
+        private void CalculateNeighbours()
+        {
+            foreach (var element in _sphereElements)
+            {
+                var center = element.Center;
+                element.Neighbours = _sphereElements
+                    .Where(other => other != element && Vector3.Distance(center, other.Center) <= 1.1f)
+                    .ToList();
+            }
+        }
     }
 }
